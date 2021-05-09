7cc04a53055714af529776e833e3dfdc307fc44d
Nikoletta Kovács <niki0505@citromail.hu>
2021-05-09 10:12:14 +0200

    add Termek class

---
+++ src/main/java/Termek.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+public class Termek {
+
+    public String pro_name;
+    public String pro_size;
+    public int pro_price;
+    public String pro_cover;
+    public int pro_pages;
+    public String pro_backcover;
+    public String pro_bindig;
+}
