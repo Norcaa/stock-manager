e72c9d5745725063f0d3de89648588c57100da0c
Nikoletta Kovács <niki0505@citromail.hu>
2021-05-09 10:13:48 +0200

    start stock updating each product

--- src/main/java/Felvetel.java
+++ src/main/java/Felvetel.java
@@ -5,12 +5,30 @@ public class Felvetel {
     public static void item(){
         System.out.println("TERMÉK MEGADÁSA...");
 
+        Termek termek = new Termek();
+        Raktar raktar = new Raktar();
+
         System.out.print("Termék neve: ");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
 
         System.out.print("Darabszáma: ");
         int n = input.nextInt();
+
+        // termék kiválasztása
+        if (name == termek.pro_name){
+            // termékhez szükséges
+            if ( termek.pro_cover == raktar.st_name){
+                raktar.st_available =- 1*n;
+            }
+            raktar.st_available =- termek.pro_pages;
+            if ( termek.pro_backcover == raktar.st_name){
+                raktar.st_available =- 1*n;
+            }
+            if ( termek.pro_bindig == raktar.st_name){
+                raktar.st_available =- 1*n;
+            }
+        }
 
         System.out.println("A megadott termék ára: ");
 
