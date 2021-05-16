e076aeb080a7ae27b7a1b901a8733515e610db5d
Nikoletta Kovács <niki0505@citromail.hu>
2021-05-16 23:13:31 +0200

    add icon

--- src/main/java/application/MyApplication.java
+++ src/main/java/application/MyApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -16,6 +17,7 @@ public class MyApplication extends Application {
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/view.fxml"));
         stage.setTitle("Raktárkezelő");
+        stage.getIcons().add(new Image(String.valueOf(MyApplication.class.getResource("/pictures/icon.png"))));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setResizable(false);
