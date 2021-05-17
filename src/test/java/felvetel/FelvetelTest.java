a8656ed55083a58e934f307c966675828186ebf3
Nikoletta Kovács <niki0505@citromail.hu>
2021-05-17 12:41:15 +0200

    update tests and add documentation

--- src/test/java/felvetel/FelvetelTest.java
+++ src/test/java/felvetel/FelvetelTest.java
@@ -5,33 +5,74 @@ import raktar.RaktarRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * {@link Felvetel} osztály metódusainak tesztje.
+ */
 public class FelvetelTest {
 
+    /**
+     * {@link Felvetel} osztály <code>getAll()</code> metódus végrehajtásának tesztje.
+     */
     @Test
     public void testGetAll() {
         assertEquals(13,Felvetel.getAll().size());
     }
 
+    /**
+     * {@link Felvetel} osztály <code>setNull()</code> metódus végrehajtásának tesztje.
+     */
+    @Test
+    public void testSetNull() {
+        Felvetel.setNull();
+        for (int i = 0; i < Felvetel.getAll().size(); i++){
+            assertEquals(0,Felvetel.getAll().get(i));
+        }
+    }
+
+    /**
+     * {@link Felvetel} osztály <code>item()</code> metódusának tesztje.
+     * <p>
+     * A teszt során egy példarendelés leadása történik.
+     * A rendelés során leadott termék nevét és darabszámát adjuk át, az
+     * <code>item()</code> metódusnak. A <code>getAll()</code> metódus
+     * segítségével lekérdezzük a megfelelő értéket.
+     */
     @Test
     public void testItem1() {
-        String termeknev = "autós";
-        int num = 2;
-        Felvetel.item(termeknev, num);
+        Felvetel.setNull();
+        Felvetel.item("autós", 2);
         assertEquals(2,Felvetel.getAll().get(1));
     }
 
+    /**
+     * {@link Felvetel} osztály <code>item()</code> metódusának tesztje.
+     * <p>
+     * A teszt során egy példarendelés leadása történik.
+     * A rendelés során leadott termék nevét és darabszámát adjuk át, az
+     * <code>item()</code> metódusnak. A <code>getAll()</code> metódus
+     * segítségével lekérdezzük a megfelelő értéket.
+     */
     @Test
     public void testItem2() {
+        Felvetel.setNull();
         String termeknev = "Harry Potter";
         int num = 4;
         Felvetel.item(termeknev, num);
         assertEquals(4,Felvetel.getAll().get(2));
     }
 
+    /**
+     * {@link Felvetel} osztály <code>pack()</code> metódusának tesztje
+     * <p>
+     * A teszt során egy példarendelés leadása történik.
+     * A rendelés során leadott termékek darabszámát adjuk át, a
+     * <code>pack()</code> metódusnak. A <code>getAll()</code> metódus
+     * segítségével lekérdezzük a megfelelő értéket.
+     */
     @Test
     public void testPack1() {
-        int num = 1;
-        Felvetel.pack(num);
+        Felvetel.setNull();
+        Felvetel.pack(1);
         assertEquals(1,Felvetel.getAll().get(6));
         assertEquals(0,Felvetel.getAll().get(7));
         assertEquals(0,Felvetel.getAll().get(8));
@@ -41,11 +82,45 @@ public class FelvetelTest {
         assertEquals(1,Felvetel.getAll().get(12));
     }
 
+    /**
+     * {@link Felvetel} osztály <code>pack()</code> metódusának tesztje
+     * <p>
+     * A teszt során egy példarendelés leadása történik.
+     * A rendelés során leadott termékek darabszámát adjuk át, a
+     * <code>pack()</code> metódusnak. A <code>getAll()</code> metódus
+     * segítségével lekérdezzük a megfelelő értéket.
+     */
     @Test
-    public void testSetNull() {
+    public void testPack2() {
         Felvetel.setNull();
-        for (int i = 0; i < Felvetel.getAll().size(); i++){
-            assertEquals(0,Felvetel.getAll().get(i));
-        }
+        Felvetel.pack(2);
+        assertEquals(0,Felvetel.getAll().get(6));
+        assertEquals(1,Felvetel.getAll().get(7));
+        assertEquals(0,Felvetel.getAll().get(8));
+        assertEquals(1,Felvetel.getAll().get(9));
+        assertEquals(1,Felvetel.getAll().get(10));
+        assertEquals(1,Felvetel.getAll().get(11));
+        assertEquals(1,Felvetel.getAll().get(12));
+    }
+
+    /**
+     * {@link Felvetel} osztály <code>pack()</code> metódusának tesztje
+     * <p>
+     * A teszt során egy példarendelés leadása történik.
+     * A rendelés során leadott termékek darabszámát adjuk át, a
+     * <code>pack()</code> metódusnak. A <code>getAll()</code> metódus
+     * segítségével lekérdezzük a megfelelő értéket.
+     */
+    @Test
+    public void testPack3() {
+        Felvetel.setNull();
+        Felvetel.pack(3);
+        assertEquals(0,Felvetel.getAll().get(6));
+        assertEquals(0,Felvetel.getAll().get(7));
+        assertEquals(1,Felvetel.getAll().get(8));
+        assertEquals(1,Felvetel.getAll().get(9));
+        assertEquals(1,Felvetel.getAll().get(10));
+        assertEquals(1,Felvetel.getAll().get(11));
+        assertEquals(1,Felvetel.getAll().get(12));
     }
 }
--- src/test/java/raktar/RaktarRepositoryTest.java
+++ src/test/java/raktar/RaktarRepositoryTest.java
@@ -12,14 +12,31 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+/**
+ * {@link RaktarRepository} osztály tesztje.
+ */
 public class RaktarRepositoryTest {
 
+    /**
+     * {@link RaktarRepository} osztály és annak a <code>getAll()</code>
+     * metódusának a tesztje.
+     */
     @Test
     public void testRaktarRepository() {
         new RaktarRepository();
         assertEquals(13, RaktarRepository.getAll().size());
     }
 
+    /**
+     * {@link RaktarRepository} osztály <code>update()</code> metódusának a tesztje.
+     * <p>
+     * A teszt során több példarendelés leadása történik.
+     * A rendelés során leadott dátumot és a termékek darabszámát adjuk át, a
+     * {@link Felvetel} osztály <code>item()</code> és <code>hozzaadas()</code>
+     * metódusának.
+     * A <code>getAll()</code> metódus segítségével lekérdezzük az értéket,
+     * amelyeknek különböznie kell a rendelés előtti értékektől.
+     */
     @Test
     public void testUpdate() throws IOException {
         new RaktarRepository();
@@ -28,9 +45,9 @@ public class RaktarRepositoryTest {
         Felvetel.item("Autós", 2);
         Felvetel.item("Harry Potter", 4);
         Felvetel.item("Star Wars", 5);
-        Felvetel.pack(1);
-        Felvetel.pack(2);
-        Felvetel.pack(3);
+        Felvetel.hozzaadas("Példa Péter", 1, 2021, 5, 14);
+        Felvetel.hozzaadas("Példa Péter", 2, 2021, 5, 8);
+        Felvetel.hozzaadas("Példa Péter", 3, 2021, 4, 16);
 
         RaktarRepository.update();
 
